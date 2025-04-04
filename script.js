aa2ab560586249600e4a178ce755e0bcba3382a6
offic <officiallyeklipse@gmail.com>
2025-04-03 22:46:09 -0400

    WIP

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-var body = document.body;
-var title = document.getElementById("title");
+const body = document.body;
+const title = document.getElementById("title");
+const cards = document.getElementsByClassName("page-card");
+
 
 function translateTitle() {
     let transformY = Math.min(this.scrollY / 2, 400);
@@ -10,4 +12,18 @@ translateTitle();
 
 window.onscroll = () => {
     translateTitle();
+}
+
+for (let i = 0; i < cards.length; i++) {
+    let card = cards[i];
+
+    card.onmouseenter = function() {
+        this.style.transform = "scale(1.03)";
+        this.style.width = "70vw";
+    }
+
+    card.onmouseleave = function() {
+        this.style.transform = "scale(1)";
+        this.style.width = "60vw";
+    }
 }
